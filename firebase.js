f79aa9a332a0cd8e25b33f78d956363e977df239
marcelotecnm <marmaiden1988@gmail.com>
2026-04-18 13:14:52 -0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,14 +3,14 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/fireba
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDzDkDovbEEDb2NlLx-VHySABSCeStxeus",
-  authDomain: "app-crud-nube-89a17.firebaseapp.com",
-  projectId: "app-crud-nube-89a17",
-  storageBucket: "app-crud-nube-89a17.firebasestorage.app",
-  messagingSenderId: "917823300201",
-  appId: "1:917823300201:web:5a510486dc7ddf03ad78f8"
+  apiKey: "AIzaSyCWtVRl1KZJcfNfQICJkbE6Y8r66opD9Xk",
+  authDomain: "serviciomma-faa93.firebaseapp.com",
+  projectId: "serviciomma-faa93",
+  storageBucket: "serviciomma-faa93.firebasestorage.app",
+  messagingSenderId: "168777569725",
+  appId: "1:168777569725:web:0b3489560022679705aed4",
+  measurementId: "G-HN98L7DMW9"
 };
-
 // INICIAR FIREBASE
 const app = initializeApp(firebaseConfig);
 
